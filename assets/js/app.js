98413e8e4353f2a5d529f51122ce4e42080f9a5f
marcelcph <55388306+marcelcph@users.noreply.github.com>
2021-12-07 21:29:07 +0100

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,12 +3,12 @@ console.log("JS works")
 function openNav() {
   document.getElementById("mobile-nav-overlay").style.height = "100%";
   document.querySelector(".mobiletest").style.overflow = "hidden";
-  document.querySelector("#notification").style.display = "none";
+  document.getElementById("notification").style.display = "none";
 }
 
 function closeNav() {
   document.getElementById("mobile-nav-overlay").style.height = "0%";
   document.querySelector(".mobiletest").style.overflow = "initial";
-  document.querySelector("#notification").style.display = "block";
+  document.getElementById("notification").style.display = "block";
 }
 
